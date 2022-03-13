fcefa044594817e644bfd91bb11fb9d527406f11
wangshb <youremail@example.com>
2022-03-13 22:49:55 +0800

    1234

---
+++ ScalaCode/src/com/aiguigu/cn/ScalaStudy.scala
@@ -0,0 +1,13 @@
+package com.aiguigu.cn
+
+object ScalaStudy {
+  def main(args: Array[String]): Unit = {
+    print("helloworld")
+
+  }
+
+  def test():Unit={
+
+  }
+
+}
---
+++ ScalaCode/src/com/aiguigu/cn/Templates.scala
@@ -0,0 +1,11 @@
+package com.aiguigu.cn
+
+/**
+ * @author wangshb
+ * @create 2022-03-13 22:04
+ */
+class Templates {
+  def main(args: Array[String]): Unit = {
+
+  }
+}
